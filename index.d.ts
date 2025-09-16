f83cd59c9e50b90cb584cd23abf499856e042494
d1vij <77372157+d1vij@users.noreply.github.com>
2025-09-16 14:00:29 +0000

    Fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare module "@d1vij/py-bridge" {
-    export function exec(filepath: string, functionName: string, kwargs: Object = {}, port: string = "0"): Promise<ExecResults>;
+    export function exec<T = string>(filepath: string, functionName: string, kwargs: Object = {}, port: string = "0"): Promise<ExecResults<T>>;
     export function install(name: string): void;
-    export type ExecResults = {
+    export type ExecResults<T> = {
         success: boolean,
         errorMsg?: string,
-        payload?: string
+        payload?: T
     }
 }
