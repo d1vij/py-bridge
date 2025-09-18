6ea2f9297387a26826eca2c4368c66492be1d799
d1vij <77372157+d1vij@users.noreply.github.com>
2025-09-18 11:47:12 +0000

    Typing fixes

--- index.d.ts
+++ index.d.ts
@@ -3,10 +3,10 @@ import { PythonPath } from "./src/ts/python-path.ts"
 declare module "@d1vij/py-bridge" {
     export function exec<T = string>(filepath: string, functionName: string, kwargs: Object = {}, port: string = "0"): Promise<ExecResults<T>>;
     export const pythonPath: PythonPath;
-    class PythonPath {
+    private class PythonPath {
         private __path: string | undefined;
         public get(): string;
-        public get(newPath: string): string;
+        public set(newPath: string): string;
     }
     export type ExecResults<T> = {
         success: boolean,
